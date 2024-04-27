5c8a7c9153a29727e8bd2f917093ca47c617ad3e
SanchoPansa <zelgadis4@gmail.com>
2024-04-27 23:07:03 +0300

    Configuration

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+  {
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    rules: {
+      "indent": [
+        "error",
+        2
+      ],
+      "quotes": [
+        "error",
+        "double"
+      ],
+      "semi": [
+        "error",
+        "always"
+      ],
+      "no-unusued-vars": [
+        "warn"
+      ]
+    }
+  },
+  pluginJs.configs.recommended,
+];
