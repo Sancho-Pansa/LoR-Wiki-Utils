f6d16b34faed4d20312c8438f686b5bcddcd0089
SanchoPansa <zelgadis4@gmail.com>
2024-05-15 00:15:28 +0300

    Составлен парсер данных из Wiki

--- src/ParseWikiData.js
+++ src/ParseWikiData.js
@@ -1,14 +1,40 @@
-async function fetchWikiData() {
-  let url = "https://leagueoflegends.fandom.com/ru/api.php?action=expandtemplates&format=json&prop=wikitext&formatversion=2&text={{%23invoke:LoRData|toJson|4}}";
-  let result = await fetch(url);
-  if(result.ok) {
-    let responseBody = await result.json();
-    let json = responseBody.expandtemplates.wikitext;
-    let wikiLoRData = JSON.parse(json);
-    for(let [key, value] of Object.entries(wikiLoRData)) {
-      console.log(key, value.name);
+/**
+ * Отправляет запрос к API League of Legends Wiki для получения данных о картах указанного сезона.
+ *
+ * Запрос состоит из вызова функции из Модуль:LoRData по преобразованию указанной таблицы Lua в JSON с возвращением результата.
+ * @param {Number} set номер сезона
+ * @returns {Object<string, Object>}
+ */
+export default async function fetchWikiData(set) {
+  let url = new URL("https://leagueoflegends.fandom.com/ru/api.php");
+  const wikiLuaCall = `{{#invoke:LoRData|toJson|${set}}}`;
+  let apiConfig = {
+    action: "expandtemplates",
+    format: "json",
+    prop: "wikitext",
+    formatversion: "2",
+    text: wikiLuaCall
+  };
+  let urlParams = new URLSearchParams(apiConfig);
+  url.search = urlParams.toString();
+  try {
+    let result = await fetch(url);
+    if(result.ok) {
+      let responseBody = await result.json();
+      let json = responseBody.expandtemplates.wikitext;
+      let wikiLoRData = JSON.parse(json);
+      return wikiLoRData;
+    } else {
+      throw new Error(`Error during Fandom API request. HTTP Code: ${result.status}`);
+    }
+  } catch(error) {
+    if(error instanceof DOMException || error instanceof TypeError) {
+      console.error("General error during Fandom API request");
+      throw error;
+    }
+    if(error instanceof SyntaxError) {
+      console.error("Error during result JSON parsing");
+      throw error;
     }
   }
 }
-
-fetchWikiData();
