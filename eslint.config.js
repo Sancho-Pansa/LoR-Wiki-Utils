29bb84aff623971c10b19baae9249e78e2474b00
SanchoPansa <zelgadis4@gmail.com>
2024-05-26 22:30:33 +0300

    ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,26 @@
 import globals from "globals";
+import stylisticJs from "@stylistic/eslint-plugin-js";
 //import pluginJs from "@eslint/js";
 
 export default [
   {
+    plugins: {
+      "@stylistic/js": stylisticJs
+    },
     languageOptions: { globals: { ...globals.browser, ...globals.node } },
     rules: {
-      "indent": [
-        "error",
-        2
-      ],
-      "quotes": [
-        "error",
-        "double"
-      ],
-      "semi": [
-        "error",
-        "always"
-      ],
-      "no-unused-vars": "warn"
+      "no-unused-vars": "warn",
+      "@stylistic/js/indent": ["error", 2],
+      "@stylistic/js/quotes": ["warn", "double"],
+      "@stylistic/js/semi": ["error", "always"],
+      "@stylistic/js/keyword-spacing": ["warn", {
+        "overrides": {
+          "if": { "after": false },
+          "for": { "after": false },
+          "while": { "after": false },
+        }
+      }]
     }
+
   }
 ];
